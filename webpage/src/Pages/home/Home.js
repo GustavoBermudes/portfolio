e15482b1f5e71dd16cedcb8b27824b2c8f3f5d3b
GustavoBermudes <106934556+GustavoBermudes@users.noreply.github.com>
2023-08-22 22:18:05 -0300

    Paralax

--- webpage/src/Pages/home/Home.js
+++ webpage/src/Pages/home/Home.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
-import Head from '../../components/head/Head'; 
+import Head from '../../components/head/Head';
 
 function Home() {
+  const [scrollOpacity, setScrollOpacity] = useState(0);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const fadeInStart = 100; 
+    const maxOpacity = 1;
+    const scrollOpacityValue = Math.min(1, (scrollPosition - fadeInStart) / 200); 
+
+    setScrollOpacity(scrollOpacityValue > 0 ? scrollOpacityValue : 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div>
       <Head />
-      <div className="home-container">
-        
-      </div>
+      <section className="full-section" style={{ opacity: scrollOpacity }}>
+      </section>
+      <section className="full-section" style={{ opacity: scrollOpacity }}>
+      </section>
     </div>
   );
 }
